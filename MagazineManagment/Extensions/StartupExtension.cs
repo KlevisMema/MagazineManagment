12b9ca3fbe1bea4aed3e219519c0485df1757e7d
Klevis Mema <memaklevis2@gmail.com>
2022-07-15 18:02:49 +1400

    AddedFolders

---
+++ MagazineManagment/Extensions/StartupExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace MagazineManagment.Web.Extensions
+{
+    public static class StartupExtension
+    {
+        public static  IServiceCollection  InjectServices(this IServiceCollection services , IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
