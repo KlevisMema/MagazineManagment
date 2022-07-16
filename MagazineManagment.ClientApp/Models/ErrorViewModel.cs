d55be369f4fb4315c4411b0acc0fcf73252b313c
Klevis Mema <memaklevis2@gmail.com>
2022-07-16 17:01:01 +1400

    Removed Client App

--- MagazineManagment.ClientApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MagazineManagment.ClientApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
