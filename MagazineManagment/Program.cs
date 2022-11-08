d513b406ae301cc6688a98bd0f32d06080d649b8
marina <memaklevis2@gmail.com>
2022-11-08 13:34:15 +0100

    Cord configuration

--- MagazineManagment/Program.cs
+++ MagazineManagment/Program.cs
@@ -4,19 +4,24 @@ using MagazineManagment.Web.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    builder =>
+    {
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    }));
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.InjectServices(builder.Configuration);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://example.com")
-                      });
-});
+
 
 var app = builder.Build();
 
@@ -32,6 +37,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
+
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
