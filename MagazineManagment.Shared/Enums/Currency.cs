3a4f3df37136ff9529337dd29ee946621982f012
Klevis Mema <memaklevis2@gmail.com>
2022-11-18 21:56:25 +0100

    Fixed return type from enum

--- MagazineManagment.Shared/Enums/Currency.cs
+++ MagazineManagment.Shared/Enums/Currency.cs
@@ -1,5 +1,8 @@
-﻿namespace MagazineManagment.Shared.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace MagazineManagment.Shared.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum CurrencyTypeEnum
     {
         Euro = 1,
